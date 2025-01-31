8994bccda3d58a345d09a5782911c7e8049c4386
ra-lin <showlinrafau@gmail.com>
2025-01-31 18:47:21 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 import os
 from flask import Flask
 from app.config import Config
-from flask_sqlalchemy import SQLAlchemy
-
-# Initialize SQLAlchemy
-db = SQLAlchemy()
 
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    # Set up default config from Config class
     app.config.from_object(Config)
 
     if test_config is not None:
@@ -18,12 +16,8 @@ def create_app(test_config=None):
     # Ensure the instance folder exists
     os.makedirs(app.instance_path, exist_ok=True)
 
-    # Initialize SQLAlchemy with the app
-    db.init_app(app)
-
     # Register the blueprint
-    from app.blueprints.snp_query import snp_bp
-    app.register_blueprint(snp_bp)
-    
+    from app.blueprints.search import search_bp
+    app.register_blueprint(search_bp)
 
     return app
