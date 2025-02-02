c6c7212a1c20f94c99ca221366ea097d99b68d53
chanksjoseph <67743208+chanksjoseph@users.noreply.github.com>
2025-02-02 23:11:33 +0000

    commented out the not-working code and reverted results.html to previous working version

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.blueprints.snp_query.views import snp_bp
 from app.blueprints.db_api.db_api import db_api
-from app.blueprints.snp_query.gene import gene_bp
+#from app.blueprints.snp_query.gene import gene_bp
 from config import Config
 
 app = Flask(__name__)
@@ -9,4 +9,4 @@ app.config.from_object(Config)
 
 app.register_blueprint(snp_bp)
 app.register_blueprint(db_api)
-app.register_blueprint(gene_bp)
+#app.register_blueprint(gene_bp)
--- app/blueprints/snp_query/gene.py
+++ app/blueprints/snp_query/gene.py
@@ -1,49 +1,49 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired, Regexp, ValidationError
-
-from ..db_api.db_api import db
-
-gene_bp = Blueprint('gene_query', __name__)
-
-def validate_search_term(form, field):  # Check if search term is valid for gene
-    print(f"Validating search term: {field.data} for type: {form.search_type.data}")
-    if not Regexp('^[A-Za-z][A-Za-z0-9_-]*$').regex.match(field.data):
-        raise ValidationError('Invalid gene name. Use only letters, numbers, underscores, or hyphens, starting with a letter.')
-
-class GeneSearchForm(FlaskForm):
-    search_term = StringField('Gene Symbol:',
-        validators=[InputRequired(), validate_search_term])
-    submit = SubmitField('Search')
-
-@gene_bp.route('/', methods=['GET', 'POST'])
-def index():
-    form = GeneSearchForm()
-    if form.validate_on_submit():
-        search_term = form.search_term.data
-        return redirect(url_for('gene_query.search_results', search_term=search_term))
-    return render_template('homepage/index.html', form=form)
-
-@gene_bp.route('/search/<search_term>')
-def search_results(search_term):
-    try:
-        
-        db.db_open()
-        
-        # Query the database for gene annotations
-        df = db.get_gene_annotations_by_gene_symbol(search_term)
-        
-        # Only fetch necessary columns: gene_symbol, gene_id, pathway, go_term, category
-        if df is not None and not df.empty:
-            gene_info = df[['gene_symbol', 'gene_id', 'pathway', 'go_term', 'category']].to_dict('records')
-            return render_template('homepage/gene_results.html', search_term=search_term, gene_info=gene_info)
-        else:
-            return f"No details found for gene {search_term}"
-
-    except Exception as e:
-        print(f"Error processing search for {search_term}: {e}")
-        return f"Error fetching data for {search_term}"
-
-    finally:
-        db.db_close()
+# from flask import Blueprint, render_template, redirect, url_for
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import InputRequired, Regexp, ValidationError
+#
+# from ..db_api.db_api import db
+#
+# gene_bp = Blueprint('gene_query', __name__)
+#
+# def validate_search_term(form, field):  # Check if search term is valid for gene
+#     print(f"Validating search term: {field.data}") #for type: {form.search_type.data}")
+#     if not Regexp('^[A-Za-z][A-Za-z0-9_-]*$').regex.match(field.data):
+#         raise ValidationError('Invalid gene name. Use only letters, numbers, underscores, or hyphens, starting with a letter.')
+#
+# class GeneSearchForm(FlaskForm):
+#     search_term = StringField('Gene Symbol:',
+#         validators=[InputRequired(), validate_search_term])
+#     submit = SubmitField('Search')
+#
+# @gene_bp.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = GeneSearchForm()
+#     if form.validate_on_submit():
+#         search_term = form.search_term.data
+#         return redirect(url_for('gene_query.search_results', search_term=search_term))
+#     return render_template('homepage/index.html', form=form)
+#
+# @gene_bp.route('/search/<search_term>')
+# def search_results(search_term):
+#     try:
+#
+#         db.db_open()
+#
+#         # Query the database for gene annotations
+#         df = db.get_gene_annotations_by_gene_symbol(search_term)
+#
+#         # Only fetch necessary columns: gene_symbol, gene_id, pathway, go_term, category
+#         if df is not None and not df.empty:
+#             gene_info = df[['gene_symbol', 'gene_id', 'pathway', 'go_term', 'category']].to_dict('records')
+#             return render_template('homepage/gene_results.html', search_term=search_term, gene_info=gene_info)
+#         else:
+#             return f"No details found for gene {search_term}"
+#
+#     except Exception as e:
+#         print(f"Error processing search for {search_term}: {e}")
+#         return f"Error fetching data for {search_term}"
+#
+#     finally:
+#         db.db_close()
