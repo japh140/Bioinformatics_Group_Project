4bca0e479c4d9723b8257c62e346f7ade3157d7e
Rafa <showlinrafau@gmail.com>
2025-02-06 11:05:40 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ try:
     # try absolute path first
     from app.blueprints.snp_query.views import snp_bp
     from app.blueprints.snp_query.gene import gene_bp
+    from app.blueprints.snp_query.download import download_bp
     from app.blueprints.db_api.db_api import db_api
 except ImportError:
     # fall back to relative path
     from blueprints.snp_query.views import snp_bp
     from blueprints.snp_query.gene import gene_bp
+    from blueprints.snp_query.download import download_bp
     from blueprints.db_api.db_api import db_api
 from config import Config
 
@@ -17,3 +19,4 @@ app.config.from_object(Config)
 app.register_blueprint(snp_bp)
 app.register_blueprint(db_api)
 app.register_blueprint(gene_bp)
+app.register_blueprint(download_bp)
