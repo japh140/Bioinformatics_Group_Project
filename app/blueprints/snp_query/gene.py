777ae3bb96d5544e9115d98dc68aa9c81c7b3693
Rafa <showlinrafau@gmail.com>
2025-02-07 12:12:39 +0000

    Update gene.py

--- app/blueprints/snp_query/gene.py
+++ app/blueprints/snp_query/gene.py
@@ -6,13 +6,14 @@ gene_bp = Blueprint('gene', __name__)
 @gene_bp.route('/gene/<gene_symbol>')
 def gene_details(gene_symbol):
     try:
-        db.db_open()
+        # Directly fetch the gene annotations using db.get_db()
         df = db.get_gene_annotations_by_gene_symbol(gene_symbol)
+        
         if df is not None and not df.empty:
             # Drop duplicates based on 'gene_symbol' (or any other column you want to avoid duplicates for)
             df = df.drop_duplicates(subset='gene_symbol', keep='first')  # Keeps the first occurrence of each gene_symbol
 
-            # Convert to list of dicts
+            # Convert to list of dicts for rendering in the template
             gene_info = df.to_dict('records')
             return render_template('gene.html', gene_symbol=gene_symbol, gene_info=gene_info)
         else:
@@ -20,5 +21,4 @@ def gene_details(gene_symbol):
     except Exception as e:
         print(f"Error fetching gene details: {e}")
         return f"Error fetching gene details for {gene_symbol}"
-    finally:
-        db.db_close()
+
