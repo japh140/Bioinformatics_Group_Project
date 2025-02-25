324950865c53e1cdadb5743b426006155991ee8c
chanksjoseph <67743208+chanksjoseph@users.noreply.github.com>
2025-02-25 17:48:39 +0000

    removed reference to deleted download.py and made some clarifications in the text in population_comparison.html

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,12 @@ try:
     # try absolute path first
     from app.blueprints.snp_query.views import snp_bp
     from app.blueprints.snp_query.gene import gene_bp
-    from app.blueprints.snp_query.download import download_bp
     from app.blueprints.snp_query.plot import plot_bp
     from app.blueprints.db_api.db_api import db_api, init_db_teardown
 except ImportError:
     # fall back to relative path
     from blueprints.snp_query.views import snp_bp
     from blueprints.snp_query.gene import gene_bp
-    from blueprints.snp_query.download import download_bp
     from blueprints.snp_query.plot import plot_bp
     from blueprints.db_api.db_api import db_api, init_db_teardown
 from app.config import Config
@@ -21,7 +19,7 @@ app.config.from_object(Config)
 app.register_blueprint(snp_bp)
 app.register_blueprint(db_api)
 app.register_blueprint(gene_bp)
-app.register_blueprint(download_bp)
+#app.register_blueprint(download_bp)
 app.register_blueprint(plot_bp)
 
 
