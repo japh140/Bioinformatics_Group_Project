7350d9b66a4220bbb0849b3a3ccbbea8ca78ae22
burhan2501 <burhangeeba@gmail.com>
2025-03-05 22:33:56 +0000

    Update remove_duplicates_from_Fst.py

--- statistics/remove_duplicates_from_Fst.py
+++ statistics/remove_duplicates_from_Fst.py
@@ -4,8 +4,8 @@ import pandas as pd
 populations = ["PJL", "BEB", "ITU", "GIH"]  
 
 # Process Each Population one at a time
-for pop in populations:
-    file_path = f"Fst_results_{pop}_CEU_annotated.xlsx"  # this brings Fst results file for all populations
+for pp in populations:
+    file_path = f"Fst_results_{pp}_CEU_annotated.xlsx"  # this brings Fst results file for all populations
     
     # Load the Fst results Excel file
     df = pd.read_excel(file_path, engine="openpyxl")
@@ -14,7 +14,7 @@ for pop in populations:
     df_cleaned = df.drop_duplicates()
 
     # Save cleaned Fst results files
-    output_file = f"Fst_results_{pop}_CEU_cleaned.xlsx"
+    output_file = f"Fst_results_{pp}_CEU_cleaned.xlsx"
     df_cleaned.to_excel(output_file, index=False, engine="openpyxl")
 
     print("Duplicates removed from Fst results files")
